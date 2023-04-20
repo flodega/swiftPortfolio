a5cf9753c89d1bc706b4155e780426c5852cb438
flodega <flodega09@yahoo.fr>
2023-04-19 19:21:45 -0500

    Initial Commit

---
+++ Tip Calculator/Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorApp.swift
+//  Tip Calculator
+//
+//  Created by florian houndjahoue on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
